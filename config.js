b9484b06345c9c47a0f05d0647e53d6c8cb860ed
SUPRIA <supriajaya02@gmail.com>
2026-06-20 14:51:06 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const firebaseConfig = { databaseURL: "https://nasukawireless-73c75-default-rtdb.asia-southeast1.firebasedatabase.app/" };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const API_URL = 'https://script.google.com/macros/s/AKfycbyaTRKbOCb3pPKdcpwFL9VQKQtib3TUR4sJbX5rLEbo4yIZheeHpdvBm5cU_Wn_pmn1/exec';
